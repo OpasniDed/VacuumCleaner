478c815df35a8fc94e3d9b9b45324e086c542f27
OpasniDed <akkbezbanov228@gmail.com>
2026-06-01 00:18:55 +0300

    Changed commands

---
+++ Commands/DropAllItemsCommand.cs
@@ -0,0 +1,48 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using Exiled.API.Features.Pickups;
+using System;
+using System.Linq;
+using VacuumCleaner.Handlers;
+using VacuumCleaner.Helpers;
+
+namespace VacuumCleaner.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    public class DropAllItemsCommand : ICommand
+    {
+        public string Command => "dropallitems";
+        public string[] Aliases => new[] { "dropitems", "di", "dropi", "drop" };
+        public string Description => Plugin.Instance.Translation.DropItemsDescription;
+
+        public bool Execute(ArraySegment<string> args, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+
+            if (player == null)
+            {
+                response = Plugin.Instance.Translation.CantUseCommand;
+                return false;
+            }
+            if (!VacuumHelper.VacuumCleaners.ContainsKey(player))
+            {
+                response = Plugin.Instance.Translation.YouAreNotVacuumCleaner;
+                return false;
+            }
+
+            if (VacuumHandler.PlayerVault[player].Count > 0)
+            {
+                foreach (ItemType item in VacuumHandler.PlayerVault[player].ToList())
+                {
+                    Pickup pickup = Pickup.CreateAndSpawn(item, player.Position);
+                    VacuumHandler.PlayerVault[player].Remove(pickup.Type);
+                }
+                response = Plugin.Instance.Translation.DroppedAllItems;
+                return true;
+            }
+
+            response = Plugin.Instance.Translation.DontHaveItems;
+            return false;
+        }
+    }
+}
---
+++ Commands/VacuumCommand.cs
@@ -0,0 +1,65 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+using VacuumCleaner.Helpers;
+
+namespace VacuumCleaner.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class VacuumCommand : ICommand
+    {
+        public string Command => "vacuum";
+        public string[] Aliases => new[] { "vac" };
+        public string Description => Plugin.Instance.Translation.VacuumCommandDescription;
+
+        private string _usage = Plugin.Instance.Translation.VacuumCommandUsage;
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+
+            if (player == null)
+            {
+                response = Plugin.Instance.Translation.CantUseCommand;
+                return false;
+            }
+
+            if (arguments.Count == 0)
+            {
+                response = _usage;
+                return false;
+            }
+
+
+            if (!int.TryParse(arguments.At(0), out int id))
+            {
+                response = Plugin.Instance.Translation.EnterPlayerId;
+                return false;
+            }
+
+            Player target = Player.Get(id);
+
+            if (target == null)
+            {
+                response = Plugin.Instance.Translation.PlayerNotFound;
+                return false;
+            }
+
+            if (VacuumHelper.DeleteVacuum(target))
+            {
+                response = string.Format(Plugin.Instance.Translation.Deleted, target.Nickname);
+                return true;
+            }
+            else if (VacuumHelper.CreateVacuum(target))
+            {
+                response = string.Format(Plugin.Instance.Translation.Created, target.Nickname);
+                return true;
+            }
+            else
+            {
+                response = "Idk";
+                return false;
+            }
+        }
+    }
+}

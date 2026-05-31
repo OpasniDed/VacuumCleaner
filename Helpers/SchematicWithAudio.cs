cb4fe6491d20cc7dbe4226da53d38b59544b991e
OpasniDed <akkbezbanov228@gmail.com>
2026-06-01 00:18:17 +0300

    Added SchematicWithAduio helper

---
+++ Helpers/SchematicWithAudio.cs
@@ -0,0 +1,10 @@
+﻿using ProjectMER.Features.Objects;
+
+namespace VacuumCleaner.Helpers
+{
+    public class SchematicWithAudio
+    {
+        public SchematicObject SchematicObject { get; set; }
+        public AudioPlayer AudioPlayer { get; set; }
+    }
+}
